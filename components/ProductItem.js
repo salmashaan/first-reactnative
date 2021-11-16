08939ef7bb0df1993e0e99479fac0322bc309261
salmashaan <smalmashaan@gmail.com>
2021-11-16 18:45:45 +0300

    pics and products showing

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { baseURL } from "../stores/instance";
 
 const ProductItem = ({ product }) => {
   return (
-    <HStack w="100%" alignItems="center" space="3">
+    <View w="100%" alignItems="center" space="3">
       <Image
         source={{
           uri: baseURL + product.image,
@@ -13,7 +13,7 @@ const ProductItem = ({ product }) => {
         style={{ width: 100, height: 100 }}
       />
       <Text>{product.name}</Text>
-    </HStack>
+    </View>
   );
 };
 
--- components/ShopItem.js
+++ components/ShopItem.js
@@ -14,7 +14,7 @@ const ShopItem = ({ shop, navigation }) => {
       <HStack w="100%" alignItems="center" space="3">
         <Image
           source={{
-            uri: shop.image,
+            uri: baseURL + shop.image,
           }}
           alt="image"
           style={{ width: 100, height: 100 }}
